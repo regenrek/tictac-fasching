d805899d93d3301e2fe07a061718be3301870b25
kevin regenrek <hello@regenrek.at>
2020-02-21 16:45:05 +0100

    ad

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,56 @@
 <template>
   <section>
-    <div>
-      <h1>Faschingsumzug Kirchberg/We
-    </div>
-    
-    <img src="https://res.cloudinary.com/dkaq2njxs/image/upload/v1582297238/IMG-20200221-WA0001_t6v7eh.jpg" />
-
+    <h1 class="title">Faschingsumzug Kirchberg/We</h1>
+    <img
+      src="https://res.cloudinary.com/dkaq2njxs/image/upload/v1582297238/IMG-20200221-WA0001_t6v7eh.jpg"
+    >
     <footer>
-      Proudly presented by <a href="https://www.macherjek.at"></a>
+      Website Proudly presented by
+      <a href="https://www.macherjek.at">macherjek.at</a>
     </footer>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import IconLink from '~/components/IconLink.vue'
+import Logo from "~/components/Logo.vue";
+import IconLink from "~/components/IconLink.vue";
 
 export default {
   components: {
     Logo,
     IconLink
   }
-}
+};
 </script>
 
 <style scoped>
+section {
+  max-width: 600px;
+}
+
+a {
+  color: #1E71B8;
+  font-weight: bold;
+}
+
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 400;
-  font-size: 100px;
   color: #2E495E;
   letter-spacing: 1px;
-  font-size: 6em;
-}
-.green {
-  color: #00C48D;
+  font-size: 2em;
+  margin-bottom: 2rem;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 3em;
-  color: #2E495E;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+img {
+  margin: 0 auto;
+  max-width: 100%;
+  display: block;
+  min-width: 370px;
 }
-
-.links {
-  padding-top: 15px;
+footer {
+  padding-top: 2rem;
 }
 </style>
